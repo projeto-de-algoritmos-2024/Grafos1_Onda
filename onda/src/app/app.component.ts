7be6bebaf09fc1ffc97eace9dc95cf3f69ca4f4c
Pablo S. Costa <pablo0359@gmail.com>
2024-04-14 09:33:35 +0000

    inicia grafo

--- onda/src/app/app.component.ts
+++ onda/src/app/app.component.ts
@@ -1,15 +1,71 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import {NoComponent} from './no/no.component';
+import { NoComponent } from './no/no.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NoComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'onda';
+  maximodenos: any;
+  larguraTela: any;
+  alturaTela: any;
+  tamanhono = 20;
+  nos: any;
+  grafo: any = [];
+  populagrafo() {
+    const colunas = this.larguraTela / (this.tamanhono - 5);
+    const linhas = this.alturaTela / (this.tamanhono - 5);
+    let cont = 0;
+    for (let i = 0; i < colunas; i++) {
+      for (let j = 0; j < linhas; j++) {
+        if (cont - colunas >= 0 && cont + colunas <= colunas - 1) {
+          if (j == 0) {
+          } else if (j == linhas - 1) {
+          } else {
+            this.grafo[cont] = [
+              cont - 1,
+              cont + 1,
+              cont + colunas,
+              cont - colunas,
+            ];
+          }
+        }else if(cont - colunas < 0 ){
+          if (j == 0) {
+          } else if (j == linhas - 1) {
+          } else {
+            this.grafo[cont] = [
+              cont - 1,
+              cont + 1,
+              cont + colunas,
+              cont - colunas,
+            ];
+          }
+        }else{
+          if (j == 0) {
+          } else if (j == linhas - 1) {
+          } else {
+            this.grafo[cont] = [
+              cont - 1,
+              cont + 1,
+              cont + colunas,
+              cont - colunas,
+            ];
+          }
+        }
+        cont++;
+      }
+    }
+  }
+  ngOnChanges() {
+    this.larguraTela = window.innerWidth / 5;
+    this.alturaTela = window.innerHeight / 5;
+    this.maximodenos = Math.trunc(this.larguraTela) * Math.trunc(alturaTela);
+    this.nos = (this.tamanhono - 5) / this.maximodenos;
+  }
 }
