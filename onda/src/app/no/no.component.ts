ed51144311713bf3ea65427341feb4d0abf5e42b
Pablo S. Costa <pablo0359@gmail.com>
2024-04-14 02:16:57 +0000

    cria componete nó

---
+++ onda/src/app/no/no.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no',
+  standalone: true,
+  imports: [],
+  templateUrl: './no.component.html',
+  styleUrl: './no.component.scss'
+})
+export class NoComponent {
+
+}
